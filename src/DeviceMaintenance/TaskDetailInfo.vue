dc79ca445326581cdff6dc81622cc427710aec1c
NJR10byh <48895838+applenjr@users.noreply.github.com>
2021-02-07 00:48:50 +0800

    更新查看任务

--- src/DeviceMaintenance/TaskDetailInfo.vue
+++ src/DeviceMaintenance/TaskDetailInfo.vue
@@ -8,85 +8,25 @@
       </el-breadcrumb-item>
     </el-breadcrumb>
     <div class="Task-container">
-      <div class="Task-box">
-        <el-form ref="form" :model="TaskInfo" label-position="left">
-          <div class="part1">
-            <el-form-item label="设备名称" class="DeviceName Device">
-              <el-input
-                v-model="TaskInfo.devicename"
-                placeholder="请输入设备名称"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="设备类别" class="DeviceClazz Device">
-              <el-input
-                v-model="TaskInfo.deviceclazz"
-                placeholder="请输入设备类别"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="保养周期" class="TaskTime">
-              每
-              <el-select
-                clearable
-                placeholder="请选择"
-                class="TaskTime-select"
-                v-model="TaskInfo.scheduleType"
-              >
-                <el-option
-                  v-for="item in tasktime"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                >
-                </el-option>
-              </el-select>
-            </el-form-item>
-          </div>
-          <div class="part2">
-            <el-form-item label="保养部位" style="width:48%">
-              <el-input
-                v-model="TaskInfo.side"
-                placeholder="请输入保养部位"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="接受标准" style="width:48%">
-              <el-input
-                v-model="TaskInfo.acceptedStandard"
-                placeholder="请输入接受标准"
-              ></el-input>
-            </el-form-item>
-          </div>
-          <div class="part2">
-            <el-form-item label="保养内容" style="width:32%">
-              <el-input
-                class="inputStyle"
-                type="textarea"
-                v-model="TaskInfo.taskcontent"
-                :rows="8"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="保养工具及备件" style="width:32%">
-              <el-input
-                class="inputStyle"
-                type="textarea"
-                v-model="TaskInfo.tasktools"
-                :rows="8"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="注意事项" style="width:32%">
-              <el-input
-                class="inputStyle"
-                type="textarea"
-                v-model="TaskInfo.attention"
-                :rows="8"
-              ></el-input>
-            </el-form-item>
-          </div>
-          <div class="Btns">
-            <div class="sub-btn">
-              <el-button @click="back">返回</el-button>
+      <div class="Task-info">
+        <div class="Name-Clazz-Time">
+          <div class="part1 Name">
+            <div class="Text">设备名称</div>
+            <div class="Info" v-for="(item, index) in deviceinfo" :key="index">
+              {{ item.deviceName }}
             </div>
           </div>
-        </el-form>
+          <div class="part1 Clazz">
+            <div class="Text">设备类别</div>
+            <div class="Info" v-for="(item, index) in deviceinfo" :key="index">
+              {{ item.deviceClazz }}
+            </div>
+          </div>
+          <div class="part1 Time">
+            <div class="Text">保养周期</div>
+            <div class="Info">{{ Time }}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -97,40 +37,25 @@ export default {
   name: "AddTaskInside",
   created: function() {
     let that = this;
-    console.log(that.$route.query);
+    // console.log(that.$route.query);
     that.taskid = that.$route.query.taskName;
   },
   data() {
     return {
-      taskid: "",
-      TaskInfo: {},
-      tasktime: [
+      taskid: "", //任务ID
+      // 设备信息
+      deviceinfo: [
         {
-          value: "Predictability",
-          label: "未知",
+          deviceName: "iPad Pro",
+          deviceClazz: "平板电脑",
         },
         {
-          value: "Yearly",
-          label: "年",
-        },
-        {
-          value: "Seasonally",
-          label: "季度",
-        },
-        {
-          value: "Monthly",
-          label: "月",
-        },
-        {
-          value: "Weekly",
-          label: "周",
-        },
-        {
-          value: "Daily",
-          label: "天",
+          deviceName: "iPad Pro",
+          deviceClazz: "平板电脑",
         },
       ],
-      value: "",
+      // 周期
+      Time: "Weekly",
     };
   },
   methods: {
@@ -147,6 +72,7 @@ export default {
 .Container-AddTaskInside {
   // border: 1px solid red;
   width: 100%;
+  height: 100%;
   .breadcrumb {
     width: 100%;
     height: 30px;
@@ -163,96 +89,47 @@ export default {
       color: #666666;
       font-weight: normal;
     }
-
     .active {
       font-size: 20px;
     }
   }
   .Task-container {
-    width: 100%;
+    // height: 80%;
     // border: 1px solid red;
     display: flex;
     justify-content: center;
-    .Task-box {
-      width: 90%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
+    .Task-info {
+      width: 85%;
+      background: #fff;
+      border-radius: 15px;
+      box-shadow: 5px 5px 20px #eeeeee, -5px 5px 20px #eeeeee;
+      padding: 10px 20px;
       // border: 1px solid red;
-      .el-form {
+      .Name-Clazz-Time {
+        padding-bottom: 10px;
+        border-bottom: 1px solid #ddd;
         display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        background: #fff;
-        border-radius: 20px;
-        box-shadow: 5px 5px 20px #eeeeee, -5px 5px 20px #eeeeee;
-        padding: 10px 30px 20px 30px;
+        justify-content: space-between;
         .part1 {
           display: flex;
-          justify-content: flex-start;
-          width: 100%;
-        }
-        .part2 {
-          // border: 1px solid red;
-          display: flex;
-          justify-content: space-between;
-          width: 100%;
-        }
-        .inputStyle {
-          width: 100%;
-        }
-        .el-form-item {
-          margin: 0;
-        }
-        .Device {
-          display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-self: flex-start;
-        }
-        // 设备名称
-        .DeviceName {
-          margin-left: 0;
-        }
-        // 设备类别
-        .DeviceClazz {
-          margin-left: 20px;
-        }
-        // 保养周期
-        .TaskTime {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-self: flex-start;
-          margin-left: 20px;
-          .TaskTime-select {
-            width: 100px;
-            margin-left: 10px;
+          align-items: flex-start;
+          .Text {
+            // border: 1px solid red;
+            font-size: 18px;
+            font-weight: 600;
+          }
+          .Info {
+            // border: 1px solid red;
+            margin-top: 10px;
+            display: flex;
+            justify-content: flex-start;
+            font-size: 14px;
+            font-weight: 400;
           }
         }
-        // 下面俩按钮
-        .Btns {
-          display: flex;
-          margin-top: 20px;
-          justify-content: center;
-          // border: 1px solid red;
-          .sub-btn {
-            .el-button {
-              width: 80px;
-              background: linear-gradient(-270deg, #6eb5fc, #409eff);
-              color: #fff;
-              border: 0;
-              padding: 10px;
-              font-size: 15px;
-              border-radius: 5px;
-              &:hover {
-                opacity: 0.9;
-              }
-            }
-          }
+        .Time {
+          margin-right: 20px;
         }
       }
     }
