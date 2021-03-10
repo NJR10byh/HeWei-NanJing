c3ddaf5c48caa2f571f3496fbe11aa9018877c91
NJR10byh <48895838+applenjr@users.noreply.github.com>
2021-03-10 22:37:38 +0800

    新增任务信息切换月份刷新当前月任务

--- src/DeviceMaintenance/TaskInformation.vue
+++ src/DeviceMaintenance/TaskInformation.vue
@@ -2,9 +2,10 @@
   <div class="Container-TaskInfomation">
     <div class="calendar">
       <FullCalendar
+        ref="fullCalendar"
         class="demo-app-calendar"
-        id="FullCalendar"
         :options="calendarOptions"
+        @changeMonth="changeMonth"
       ></FullCalendar>
     </div>
   </div>
@@ -17,19 +18,21 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
-// import jquery from "jquery";
-
-document.getElementsByClassName("fc-next-button").onclick = function() {
-  console.log("aaa");
-};
 
 export default {
   name: "DeviceInformation",
   components: {
     FullCalendar,
   },
+  mounted() {
+    // 这里有两点要注意，想要调用插件的方法，要在组件上设置ref
+    // 并且在组件未加载的时候this.$refs中是没有fullCalendar的，所以未加载的时候调用方法会报错
+    this.calendarApi = this.$refs.fullCalendar.getApi();
+  },
   created() {
     let that = this;
+    // console.log(new Date().getMonth() + 1);
+    that.thismonth = new Date().getMonth() + 1;
     axios.get("http://47.102.214.37:8080/user/me").then((res) => {
       console.log(res.data);
     });
@@ -51,6 +54,9 @@ export default {
   },
   data() {
     return {
+      thismonth: null, // 当前月
+
+      calendarApi: null,
       calendarOptions: {
         plugins: [
           dayGridPlugin,
@@ -63,28 +69,96 @@ export default {
           center: "title",
           right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
         },
-        events: [
-          // put the array in the `events` property
-          {
-            title: "event1",
-            start: "2021-03-04",
-            backgroundColor: "#fa5555",
-            borderColor: "#fa5555",
-            textColor: "#fff",
-          },
-        ],
+        buttonText: {
+          // 文本将显示在headerToolbar / footerToolbar的按钮上。不支持HTML注入。所有特殊字符将被转义。
+          today: "今天",
+          month: "月",
+          week: "周",
+          day: "天",
+        },
+        events: [],
         initialView: "dayGridMonth",
         editable: true,
         weekends: true,
         dayMaxEvents: true,
         navLinks: true,
         eventClick: this.handleEventClick,
+        locale: "zh-cn",
         height: "100%",
-        /* you can update a remote database when these fire:
-        eventAdd:
-        eventChange:
-        eventRemove:
-        */
+        customButtons: {
+          // 定义可在headerToolbar / footerToolbar中使用的自定义按钮
+          prev: {
+            // 上个月
+            text: "PREV",
+            click: () => {
+              this.calendarApi.prev();
+              let that = this;
+              that.thismonth--;
+              let url =
+                "http://47.102.214.37:8080/ops/dates/2021/" + that.thismonth;
+              axios.get(url).then((res) => {
+                console.log(res);
+                for (let i = 0; i < res.data.length; i++) {
+                  that.calendarOptions.events.push({
+                    id: res.data[i].schedule.id,
+                    title: res.data[i].schedule.name,
+                    start: res.data[i].date,
+                    backgroundColor: "#409eff",
+                    borderColor: "#409eff",
+                    textColor: "#fff",
+                  });
+                }
+              });
+            },
+          },
+          next: {
+            // 下个月
+            text: "NEXT",
+            click: () => {
+              this.calendarApi.next();
+              let that = this;
+              that.thismonth++;
+              let url =
+                "http://47.102.214.37:8080/ops/dates/2021/" + that.thismonth;
+              axios.get(url).then((res) => {
+                console.log(res);
+                for (let i = 0; i < res.data.length; i++) {
+                  that.calendarOptions.events.push({
+                    id: res.data[i].schedule.id,
+                    title: res.data[i].schedule.name,
+                    start: res.data[i].date,
+                    backgroundColor: "#409eff",
+                    borderColor: "#409eff",
+                    textColor: "#fff",
+                  });
+                }
+              });
+            },
+          },
+          today: {
+            text: "今天",
+            click: () => {
+              this.calendarApi.today();
+              let that = this;
+              that.thismonth = new Date().getMonth() + 1;
+              let url =
+                "http://47.102.214.37:8080/ops/dates/2021/" + that.thismonth;
+              axios.get(url).then((res) => {
+                console.log(res);
+                for (let i = 0; i < res.data.length; i++) {
+                  that.calendarOptions.events.push({
+                    id: res.data[i].schedule.id,
+                    title: res.data[i].schedule.name,
+                    start: res.data[i].date,
+                    backgroundColor: "#409eff",
+                    borderColor: "#409eff",
+                    textColor: "#fff",
+                  });
+                }
+              });
+            },
+          },
+        },
       },
       currentEvents: [],
 
@@ -102,6 +176,15 @@ export default {
         path: "./taskDetailInfo",
         query: obj,
       });
+    },
+    // 选择月份
+    changeMonth(start, end, current) {
+      console.log(
+        "changeMonth",
+        start.format(),
+        end.format(),
+        current.format()
+      );
     },
   },
 };
